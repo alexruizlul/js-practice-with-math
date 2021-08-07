19f241f7d54fa2b8ba5b477c65e70f82a99b53ce
alexruizlul <alex007ruiz@gmail.com>
2021-08-07 12:42:40 -0500

    created the logic necessary to calculate the analysis of salary

---
+++ analisis.js
@@ -0,0 +1,66 @@
+// Helpers
+function esPar(numero) {
+    return (numero % 2 === 0);
+}
+  
+function calcularMediaAritmetica(lista) {
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+
+// Calculadora de mediana
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length / 2);
+  
+    if (esPar(lista.length)) {
+      const personaMitad1 = lista[mitad - 1];
+      const personaMitad2 = lista[mitad];
+  
+      const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2]);
+      return mediana;
+    } else {
+      const personaMitad = lista[mitad];
+      return personaMitad;
+    }
+}
+
+// Mediana General
+const salariosMx = mexico.map(
+    function (persona) {
+        return persona.salary;
+    }
+);
+
+const salariosMxSorted = salariosMx.sort(
+    function (salaryA, salaryB) {
+        return salaryA - salaryB;
+    }
+);
+
+
+
+const medianaGeneralMx = medianaSalarios(salariosMxSorted);
+
+// console.log(medianaGeneralMx);
+
+// Mediana del top 10%
+const spliceStart = (salariosMxSorted.length * 90) / 100;
+const spliceCount = salariosMxSorted.length - spliceStart;
+
+const salariosMxTop10 = salariosMxSorted.splice(
+  spliceStart,
+  spliceCount,
+);
+
+const medianaTop10Mx = medianaSalarios(salariosMxTop10);
+
+console.log({
+  medianaGeneralMx,
+  medianaTop10Mx,
+});
